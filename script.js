cd5de0636273c9897c6c0076d6a74cd114fbe2fd
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-29 01:38:13 +0600

    Learn to hide and display element

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("mouseleave", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
 
 })
