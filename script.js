9546c6d239c075d1ae112c4f219b4894061ca667
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-29 02:23:28 +0600

    Eventlistener for deposit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//login button event handler
+
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("mouseleave", function () {
     const loginArea = document.getElementById("login-area");
@@ -5,4 +7,16 @@ loginBtn.addEventListener("mouseleave", function () {
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 
+})
+
+
+//deposit button event handler
+
+const depositBtn = document.getElementById("deposit");
+
+depositBtn.addEventListener("click", function() {
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+    console.log(depositNumber);
+    
 })
