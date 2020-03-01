9a17b6ab09751801f276d125fc44be7fe2870b62
Abdur Rakib <43021476+RakibWebDeveloper@users.noreply.github.com>
2020-03-01 08:06:59 +0600

    Added click

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //login button event handler
 
 const loginBtn = document.getElementById("login");
-loginBtn.addEventListener("mouseleave", function () {
+loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
